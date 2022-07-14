419849b6512535a5717f2907dc964ac75b8b54e1
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-07-14 14:42:21 +0700

    Added NFT components

---
+++ src/components/NFT.vue
@@ -0,0 +1,82 @@
+<template>
+    <a :href="`/asset/${nft.assetId}`" class="nft">
+        <img v-if="nft.metadata.url" :src="nft.metadata.url" :alt="nft.name"  class="nft__img"/>
+        <p v-else class="nft__img--empty">?</p>
+        <div class="nft__info">
+            <div class="nft__name">
+                <p>{{ nft.name }}</p>
+            </div>
+            <div v-if="nft.price" class="nft__price">
+                <p>{{ nft.price }} <img src="/img/waves-token.svg" /></p>
+            </div>
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "NFT",
+    props: ["nft"],
+};
+</script>
+
+<style scoped>
+.nft {
+    width: 357px;
+    height: 450px;
+    margin-top: 20px;
+}
+
+.nft,
+.nft:hover,
+.nft:active {
+    color: black;
+    text-decoration: none;
+}
+
+.nft__img, .nft__img--empty {
+    width: 375px;
+    margin: auto;
+    border-radius: 18px;
+    box-shadow: 2px 2px 2px 0px rgb(206, 206, 206),
+        -2px -2px 2px 0px rgba(255, 255, 255, 0.5);
+}
+
+.nft__img {
+    height: 375px;
+}
+
+.nft__img--empty {
+    padding: 109px 0px;
+    font-size: 130px;
+    text-align: center;
+}
+
+.nft__info {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nft__name {
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 27px;
+}
+
+.nft__price {
+    font-weight: 300;
+}
+
+.nft__price > p {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.nft__price > p > img {
+    margin-left: 10px;
+}
+</style>
