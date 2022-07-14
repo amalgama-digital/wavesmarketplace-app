a7c819f7034f4296d43f6e32cf55efa0715a876c
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-07-14 14:42:46 +0700

    Update news page

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="news">
-        <div class="news-articles">
-            <a v-for="(article) in articles" v-bind:key="article.title" :href="article.guid" class="news-article">
-                <img :src="article.thumbnail">
-                <p class="news-article-title">{{ article.title }}</p>
-                <p class="news-article-date">{{ new Date(article.pubDate).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' }) }}</p>
+        <div class="news__articles">
+            <a :href="article.guid" class="article" v-for="article in articles" v-bind:key="article.title">
+                <img :src="article.thumbnail" />
+                <p class="article__title">{{ article.title }}</p>
+                <p class="article__date">
+                    {{
+                        new Date(article.pubDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                        })
+                    }}
+                </p>
             </a>
         </div>
     </div>
@@ -15,60 +23,65 @@ export default {
     name: "News",
     data() {
         return {
-            articles: []
-        }
+            articles: [],
+        };
     },
     async mounted() {
         try {
-            let res = await this.$http.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40wavespunks');
+            let res = await this.$http.get(
+                "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40wavespunks"
+            );
             if (res.status == 200) {
                 this.articles = res.data.items;
             }
         } catch (error) {
             console.error(error);
         }
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
-    .news {
-        margin: 55px;
-    }
+.news {
+    margin: 55px;
+}
 
-    .news-articles {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-        grid-gap: 32px;
-        grid-auto-flow: dense;
-        max-width: 1200px;
-        margin: auto;
-    }
+.news__articles {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 32px;
+    grid-auto-flow: dense;
+    max-width: 1200px;
+    margin: auto;
+}
 
-    .news-article:nth-child(31n + 1) {
-        grid-column: 1 / -2;
-    }
+.article:nth-child(31n + 1) {
+    grid-column: 1 / -2;
+}
 
-    .news-article, .news-article:hover, .news-article:active {
-        text-decoration: none;
-        color: black;
-    }
+.article,
+.article:hover,
+.article:active {
+    text-decoration: none;
+    color: black;
+}
 
-    .news-article, .news-article > img {
-        width: 100%;
-    }
+.article,
+.article > img {
+    width: 100%;
+}
 
-    .news-article-title {
-        color: #ff00ce !important;
-        font-size: 20px;
-        font-weight: 500;
-        margin-top: 10px;
-        text-overflow: clip;
-        width: 100%;
-    }
+.article__title {
+    width: 100%;
+    margin-top: 10px;
+    color: #0055FF !important;
+    font-size: 20px;
+    font-weight: 500;
+    text-overflow: clip;
+}
 
-    .news-article-date {
-        font-size: 14px;
-        margin-top: -10px;
-    }    
+.article__date {
+    margin-top: -10px;
+    font-size: 14px;
+}
 </style>
