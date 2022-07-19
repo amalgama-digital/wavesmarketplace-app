325fb00396d4ca0353210c80aad058c101752780
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-07-19 11:08:33 +0700

    Update front page

--- src/pages/Front.vue
+++ src/pages/Front.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="wavesmarketplace-home">
+    <div class="home">
         <header>
-            <div class="wavesmarketplace-logo">
+            <div class="header__logo">
                 <a href="/">
-                    <img src="/img/logo-black.png">
+                    <img src="/img/header/logo.svg" />
                 </a>
             </div>
-            <div v-if="authorized" class="wavesmarketplace-links">
+            <div v-if="authorized" class="header__links">
                 <a href="/news">NEWS</a>
-                <a href="/myNFT">MY NFT</a>
+                <a :href="`/user/${wallet.address}`">MY NFT</a>
                 <a href="#" @click="logout">LOG OUT</a>
             </div>
-            <div v-else-if="!authorized" class="wavesmarketplace-links">
+            <div v-else-if="!authorized" class="header__links">
                 <a href="/news">NEWS</a>
                 <a href="#" @click="connect = true">LOG IN</a>
             </div>
@@ -20,181 +20,200 @@
             <router-view></router-view>
         </main>
         <footer>
-            <div class="wavesmarketplace-footer-wrapper">
-                <h2>GET ON THE LIST</h2>
-                <p>Send a request and get an early access for the Waves Marketplace</p>
-                <button @click="care">EARLY ACCESS</button>
-                <div class="wavesmarketplace-links">
+            <div class="footer__text">NFT TSUNAMI IS HERE</div>
+            <div class="footer__blocks">
+                <div class="footer__logo">
+                    <a href="/">
+                        <img src="/img/footer/logo.svg" />
+                    </a>
+                </div>
+                <div class="footer__links">
                     <a id="discord" target="_blank" href="https://discord.gg/gfpKDfRtvf">
-                        <img src="/img/discord-footer.svg">
+                        <img src="/img/footer/discord.svg" />
                     </a>
                     <a id="telegram" target="_blank" href="https://t.me/wavespunks_en">
-                        <img src="/img/telegram-footer.svg">
+                        <img src="/img/footer/telegram.svg" />
                     </a>
                     <a id="instagram" target="_blank" href="https://instagram.com/waves.punks">
-                        <img src="/img/instagram-footer.svg">
+                        <img src="/img/footer/instagram.svg" />
+                    </a>
+                </div>
+                <div class="footer__company">
+                    <a href="https://wavesassociation.org/">
+                        <img src="/img/footer/wa.svg" />
+                    </a>
+                    <a href="https://picsell.art/">
+                        <img src="/img/footer/psc.svg" />
                     </a>
                 </div>
             </div>
         </footer>
-        <connect-wallet v-if="connect" :connect="connect" v-on:close="connect = $event" v-on:success="login()"></connect-wallet>
+        <connect-wallet
+            :connect="connect"
+            v-if="connect"
+            v-on:close="connect = $event"
+            v-on:success="login()"
+        ></connect-wallet>
     </div>
 </template>
 
 <script>
-    import ConnectWallet from "../components/ConnectWallet.vue";
+import ConnectWallet from "../components/ConnectWallet.vue";
 
-    export default {
-        name: "Front",
-        data() {
-            return {
-                authorized: false,
-                connect: false
-            }
-        },
-        components: {
-            ConnectWallet
-        },
-        mounted() {
-            const data = window.localStorage.getItem("loginChoice");
-            if (!data) {
-                this.authorized = false;
-            } else {
-                this.authorized = true;
-            }
-        },
-        methods: {
-            login() {
-                this.authorized = true;
-            },
-
-            logout() {
-                window.localStorage.removeItem("loginChoice");
-                location.reload();
-            },
-
-            care() {
-                window.location.href = "mailto:care@wavesmarketplace.com";
-            }
+export default {
+    name: "Front",
+    data() {
+        return {
+            authorized: false,
+            connect: false,
+            wallet: {},
+        };
+    },
+    components: {
+        ConnectWallet,
+    },
+    mounted() {
+        const data = window.localStorage.getItem("loginChoice");
+        if (!data) {
+            this.authorized = false;
+        } else {
+            this.authorized = true;
+            this.wallet = JSON.parse(data);
         }
-    }
+    },
+    methods: {
+        login() {
+            this.authorized = true;
+        },
+
+        logout() {
+            window.localStorage.removeItem("loginChoice");
+            location.reload();
+        },
+    },
+};
 </script>
 
 <style scoped>
-    @media only screen and (max-width: 768px) {
-        header {
-            flex-direction: column;
-            justify-content: center !important;
-            align-items: center !important;
-        }
-
-        header > .wavesmarketplace-links {
-            margin-top: 20px;
-        }
+@media only screen and (max-width: 1140px) {
+    .footer__text {
+        font-size: 6.1vw !important;
     }
+}
 
-    @media only screen and (max-width: 640px) {
-        .wavesmarketplace-logo img {
-            width: 300px !important;
-        }
-
-        .wavesmarketplace-footer-wrapper > h2 {
-            font-size: 25px !important;
-            line-height: 32px !important;
-        }
-
-        .wavesmarketplace-footer-wrapper > p {
-            text-align: center;
-            font-size: 14px !important;
-            line-height: 18px !important;
-        }
-    }
-
-    @media only screen and (max-width: 500px) {
-        .wavesmarketplace-logo img {
-            width: 225px !important;
-        }
-    }
-
+@media only screen and (max-width: 768px) {
     header {
-        display: flex;
-        justify-content: space-between;
-        margin: 55px;
-    }
-
-    main {
-        min-height: calc(100vh - 298px);
-    }
-
-    .wavesmarketplace-home {
-        font-family: Inter;
-        font-style: normal;
-    }
-
-    .wavesmarketplace-logo img {
-        width: 400px;
-    }
-
-    .wavesmarketplace-links {
-        display: flex;
-        align-items: center;
-    }
-
-    .wavesmarketplace-links a, .wavesmarketplace-links a:hover, .wavesmarketplace-links a:active {
-        text-decoration: none;
-        color: black;
-    }
-
-    .wavesmarketplace-links > a {
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 17px;
-        margin: 0px 15px;
-    }
-
-    footer {
-        font-family: Inter;
-        font-style: normal;
-        background: black;
-        color: white;
-        height: 500px;
-    }
-
-    .wavesmarketplace-footer-wrapper {
-        display: flex;
         flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        /* background: radial-gradient(46.38% 222.62% at 47.18% 167.45%, #7915A8 0.22%, rgba(0, 85, 255, 0) 100%); */
-        background: radial-gradient(48.56% 121.64% at 48.37% 117%, #7915A8 0.22%, rgba(0, 85, 255, 0) 100%);
+        justify-content: center !important;
+        align-items: center !important;
     }
 
-    .wavesmarketplace-footer-wrapper > h2 {
-        font-weight: 500;
-        font-size: 35px;
-        line-height: 42px;
+    .header__logo img {
+        width: 300px !important;
     }
 
-    .wavesmarketplace-footer-wrapper > p {
-        font-weight: 300;
-        font-size: 18px;
-        line-height: 22px;
+    .header__links {
+        margin-top: 20px;
     }
 
-    .wavesmarketplace-footer-wrapper > button {
-        background: #FFD645;
-        border-radius: 8px;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 22px;
-        padding: 10px 35px;
-        margin: 50px 0;
-        border: 0;
-        box-shadow: 2px 2px 2px 0px rgba(255, 214, 69, 0.6);
+    .footer__blocks {
+        flex-direction: column !important;
     }
 
-    .wavesmarketplace-footer-wrapper > button:hover {
-        cursor: pointer;
+    .footer__links {
+        margin: 20px 0px;
     }
+}
+
+@media only screen and (max-width: 640px) {
+    /* .header__logo img {
+        width: 300px !important;
+    } */
+}
+
+@media only screen and (max-width: 500px) {
+    .header__logo img {
+        width: 225px !important;
+    }
+}
+
+@media only screen and (max-width: 425px) {
+    .header__links > a {
+        margin: 0px 5px !important;
+    }
+
+    .footer__text {
+        font-size: 5.1vw !important;
+    }
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    margin: 65px;
+}
+
+main {
+    min-height: calc(100vh - 298px);
+}
+
+footer {
+    margin: 65px;
+    margin-bottom: 25px;
+}
+
+.home {
+    font-family: Inter;
+    font-style: normal;
+}
+
+.header__links {
+    display: flex;
+    align-items: center;
+}
+
+.header__links a,
+.header__links a:hover,
+.header__links a:active {
+    text-decoration: none;
+    color: black;
+}
+
+.header__links > a {
+    margin: 0px 15px;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+}
+
+.footer__text {
+    font-weight: 900;
+    font-size: 8.1vw;
+    line-height: 182px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #f0f0f3;
+    text-shadow: 3px 3px 5px rgba(9, 12, 31, 0.25),
+        -3px -3px 6px rgba(255, 255, 255, 0.6);
+}
+
+.footer__blocks {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.footer__links > a:first-child,
+.footer__links > a:nth-child(2) {
+    margin-right: 10px;
+}
+
+.footer__company {
+    display: flex;
+}
+
+.footer__company > a:first-child {
+    margin-right: 10px;
+}
 </style>
